bb5626ca44bc8561b42fa3860253a02a0624988c
Garande <hassaniwafula@gmail.com>
2020-11-17 23:56:09 +0300

    creation of websocket

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap'
+import { useState, useEffect } from 'react';
+import { socket } from './utils/configs';
+
+
 
 function App() {
+
+  // const [socket, createSocket] = useState();
+
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    createConnection()
+  }, [])
+
+  const createConnection = () => {
+    socket.addEventListener('open', event => {
+      console.log('Connected')
+    });
+
+    socket.addEventListener('message', event => {
+      console.log(event.data);
+    });
+
+    socket.addEventListener('close', event => {
+      console.log('Connection closed')
+    });
+
+    socket.addEventListener('error', event => {
+      console.error(event.data)
+    })
+  }
+
+
+  const handleOnChange = (e) => {
+    setMessage(e.target.value)
+  }
+
+
+  const sendMessage = () => {
+    socket.send(message)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" style={{ maxHeight: '100px' }} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input name="message" onChange={(e) => handleOnChange(e)} />
+        <button style={{ height: '40px', margin: '20px' }} onClick={(e) => sendMessage()}>Send</button>
+        <button style={{ height: '45px' }} onClick={() => createConnection()}>
+          Connect
+        </button>
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -5,12 +5,42 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const cors = require('cors');
+const http = require('http')
 
 app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
+
+
+const WebSocket = require('ws');
+// const { connection } = require('mongoose');
+// const server = http.createServer(app);
+
+const ws = new WebSocket.Server({ port: 5001 });
+
+
+ws.on('connection', function connection(wsConnection) {
+
+    wsConnection.on('message', message => {
+        console.log(`Server received: ${message}`);
+
+
+        //Broadcasting the message
+        ws.clients.forEach(client => {
+            if (client != wsConnection) {
+                // console.log(client);
+                // wsConnection.send(client)
+                // client.send(`You sent: ${message}`)
+            }
+        })
+    })
+
+
+});
+
+
 
 
 //Routes
